2554b4ca058e05a6cc6990475589349d256feb12
Kush Han <kush@justx.finance>
2024-12-19 20:43:30 +0900

    CryptoSwift .upToNextMinor(from: "1.5.1")

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-http-types.git", from: "1.1.0"),
         .package(url: "https://github.com/Electric-Coin-Company/MnemonicSwift.git", from: "2.2.4"),
         .package(url: "https://github.com/GigaBitcoin/secp256k1.swift.git", from: "0.17.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.8.2")
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.5.1"))
     ],
     targets: [
         // MARK: - Targets
